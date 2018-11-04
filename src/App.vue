d232ab822d2ad80fa3e38628104e5c18675767a8
ManzDev <JoseRomanHernandez@gmail.com>
2018-11-04 01:38:41 +0000

    VueJS parcel template

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>¡VueJS 2.0!</h1>
+    <img src="./assets/logo.png" alt="VueJS Logo">
+  </div>
+</template>
+
+<style>
+  :root {
+    text-align: center;
+  }
+  
+  h1 {
+    font-family: Montserrat, Impact, sans-serif;
+    font-size: 72px;
+  }
+</style>
