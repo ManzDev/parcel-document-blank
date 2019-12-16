f50a34c4a4c07586577a70fd1baa0242c1fb7046
ManzDev <JoseRomanHernandez@gmail.com>
2019-12-16 13:22:58 +0000

    Update vue config

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div>
-    <h1>¡VueJS 2.0!</h1>
-    <img src="./assets/logo.png" alt="VueJS Logo">
+    <h1>¡VueJS 2.x!</h1>
+    <img src="./assets/logo.png" alt="VueJS Logo" />
   </div>
 </template>
 
-<style>
-  :root {
-    text-align: center;
-  }
+<script>
+export default {
+  name: 'App'
+};
+</script>
 
-  h1 {
-    font-family: Montserrat, Impact, sans-serif;
-    font-size: 72px;
-  }
+<style>
+:root {
+  text-align: center;
+}
+
+h1 {
+  font-family: Montserrat, Impact, sans-serif;
+  font-size: 72px;
+}
 </style>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue';
 import App from './App.vue';
 
+Vue.config.devtools = true;
+
 new Vue({
   render: h => h(App)
 }).$mount('#app');
